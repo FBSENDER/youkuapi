af3b814bea59c6030eb9f1e41529cc4b0073184b
刘梦晨 <mengchen.liu@corp.elong.com>
2016-03-29 14:42:19 +0800

    bug fixed

--- lib/youkuapi/comments_api.rb
+++ lib/youkuapi/comments_api.rb
@@ -1,10 +1,12 @@
 module Youkuapi
   class Comment
-
-    include Youkuapi
-
-    def show
+    def get_by_video(video_id, client_id)
+      uri = "https://openapi.youku.com/v2/comments/by_video.json"
+      HTTParty.get(uri, query: {client_id: client_id, video_id: video_id})
     end
-
+    def hot_get_by_video(video_id, client_id)
+      uri = "https://openapi.youku.com/v2/comments/hot/by_video.json"
+      HTTParty.get(uri, query: {client_id: client_id, video_id: video_id})
+    end
   end
 end
--- lib/youkuapi/videos_api.rb
+++ lib/youkuapi/videos_api.rb
@@ -1,14 +1,14 @@
 module Youkuapi
   class Video
 
-    include Youkuapi
-
     def show_basic(video_id, client_id)
-      HTTParty.get(get_uri, query:{client_id: client_id, video_id: video_id})
+      uri = "https://openapi.youku.com/v2/videos/show_basic.json"
+      HTTParty.get(uri, query:{client_id: client_id, video_id: video_id})
     end
 
     def show(video_id, client_id)
-      HTTParty.get(get_uri, query:{client_id: client_id, video_id: video_id})
+      uri = "https://openapi.youku.com/v2/videos/show.json"
+      HTTParty.get(uri, query:{client_id: client_id, video_id: video_id})
     end
 
   end
